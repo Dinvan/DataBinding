d9d9bc757a14fee3589418f0cfbb50387a6be02c
Dinesh Bagvan <dinesh.b@advanz101.com>
2019-08-02 14:59:20 +0530

    test c

---
+++ app/src/main/java/com/example/databinding/SelectC.java
@@ -0,0 +1,13 @@
+package com.example.databinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_user_c);
+    }
+}
